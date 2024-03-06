1dfefffcc660ba52b076aa50058fe80c3d35b117
Elen Gaslin <lenokverba@gmail.com>
2024-03-06 16:04:30 +0200

    Gasanova

---
+++ js/modul1-1/task2.js
@@ -0,0 +1,9 @@
+// Обчислити бонус для працівника зарплати, якщо він працює більше 40 годин на тиждень.
+// Бонус дорівнює 20 % від зарплати за додаткові години.(використати тернарний оператор)
+
+const salary = Number(prompt(`Enter yor salary`));
+console.log(typeof salary);
+const bonus = (20 / 100) * salary;
+const hours = 40;
+const totalSalary = bonus + salary;
+console.log(totalSalary);
