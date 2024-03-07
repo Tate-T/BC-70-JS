d4317fc0410c3f3d378737fef9223efa019b32d0
Dimka863 <dlysachenko98@gmail.com>
2024-03-07 13:58:57 +0100

    task4-done

---
+++ JS/module-1.2/task4.js
@@ -0,0 +1,13 @@
+// Змінна SAVED_PASSWORD зберігає значення попередньо збереженого пароля. Попросити в користувача ввести пароль та перевірити чи він правильний.
+
+const SAVED_PASSWORD = "qwerty12345";
+
+function passChecker(password) {
+  if (password === SAVED_PASSWORD) {
+    return "Successfuly logged in!";
+  } else {
+    return "Wrong password :(";
+  }
+}
+
+alert(passChecker("qwerty145"));
