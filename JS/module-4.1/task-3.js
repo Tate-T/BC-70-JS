72a4c8fe3212e184f5f66d9330ccef605217241f
Yana <yanassam81@gmail.com>
2024-03-15 14:27:29 +0100

    task-3

---
+++ JS/module-4.1/task-3.js
@@ -0,0 +1,12 @@
+const userBlock = "curs";
+
+// const user ={
+//   'location':'Kyiv',
+//   block:'JavaScript',
+// }
+
+const user = {
+  location: "Kyiv",
+  [userBlock]: "JavaScript",
+};
+console.log(user);
