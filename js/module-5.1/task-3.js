44712569d6e7df478528dbe0eebe052a208bf174
Valentyn-Melnyk <v.iv.melnyk@gmail.com>
2024-03-20 15:26:58 +0200

    task-3

---
+++ js/module-5.1/task-3.js
@@ -0,0 +1,85 @@
+// Отримати масив імен всіх користувачів (поле name).
+
+// const developersTeam = [
+//   {
+//     nameUser: "Yana",
+//     location: "Poland",
+//   },
+//   {
+//     nameUser: "Dima",
+//     location: "Poland",
+//   },
+//   {
+//     nameUser: "Artem",
+//     location: "Ukraine",
+//   },
+//   {
+//     nameUser: "Vitaliy",
+//     location: "Ukraine",
+//   },
+//   {
+//     nameUser: "Valentyn",
+//     location: "Ukraine",
+//   },
+// ];
+
+// const designTeam = [
+//   {
+//     nameUser: "Anna",
+//     location: "Poland",
+//   },
+//   {
+//     nameUser: "Dima",
+//     location: "Poland",
+//   },
+//   {
+//     nameUser: "Artem",
+//     location: "Ukraine",
+//   },
+//   {
+//     nameUser: "Vitaliy",
+//     location: "Ukraine",
+//   },
+//   {
+//     nameUser: "Valentyn",
+//     location: "Ukraine",
+//   },
+// ];
+
+// // явне повернення
+// const createdArrayNames = (arrayNamesFun) => {
+//   const arrayNames = arrayNamesFun.map((keyUser) => keyUser.nameUser);
+//   return arrayNames;
+// };
+// console.log(createdArrayNames(developersTeam));
+// console.log(createdArrayNames(designTeam));
+
+// неявне повернення
+// const createdArrayNames = (arrayNamesFun) =>
+//   arrayNamesFun.map((keyUser) => keyUser.nameUser);
+// console.log(createdArrayNames(developersTeam));
+
+const product = ["🍎", "🍇", "🍋", "🍓"];
+
+const products = [
+  {
+    productName: "🍎",
+    price: 5,
+    quantity: 8,
+  },
+  {
+    productName: "🍇",
+    price: 2,
+    quantity: 6,
+  },
+  {
+    productName: "🍋",
+    price: 4,
+    quantity: 6,
+  },
+  {
+    productName: "🍓",
+    price: 1,
+    quantity: 3,
+  },
+];
